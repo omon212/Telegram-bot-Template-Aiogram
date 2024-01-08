4e184638a5bd3e381b30c321a0941dbe80387492
omon212 <omonnuloraimkulov@gmail.com>
2024-01-08 17:43:03 +0500

    first commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,7 +3,6 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
 
-
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
